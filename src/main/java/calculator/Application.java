a4995a78502a53c9e87aec252287e5b3c7257ec3
juns0720 <juns0720@naver.com>
2025-10-18 00:13:29 +0900

    feat(calculator): implement parse input
    check customSeparator at the front of the input
    number parsing through separator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,8 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Application {
 
     private static final String INPUT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
@@ -15,7 +17,7 @@ public class Application {
         }
 
         Calculator calculator = new Calculator();
-
+        List<String> parsedNumber = calculator.parse(input);
 
 
     }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,10 +6,33 @@ import java.util.List;
 
 public class Calculator {
 
-    private List<String> separators = new ArrayList<String>(Arrays.asList(",",";"));
+    private List<String> separators = new ArrayList<String>(Arrays.asList(",",":"));
 
     public void addCustomSeparator(String separator) {
         separators.add(separator);
     }
+
+    public List<String> parse(String input){
+
+        //커스텀 구분자 검사
+        if (input.startsWith("//")){
+            int endIndex = input.indexOf("\\n");
+            if (endIndex == -1){
+                throw new IllegalArgumentException();
+            }
+
+            String customSeparator = input.substring(2,endIndex);
+            addCustomSeparator(customSeparator);
+            input = input.substring(endIndex+2);
+        }
+        //구분자를 통해 숫자 파싱
+        return List.of(input.split(String.join("|", separators)));
+
+
+
+    }
+
+
+
 
 }
